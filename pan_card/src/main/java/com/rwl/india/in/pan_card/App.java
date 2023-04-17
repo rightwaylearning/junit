6477569713f168df99eb51877c0af5ffa8bc7a39
right way learning <java.development.pune@gmail.com>
2023-04-17 09:15:03 +0530

    data

---
+++ pan_card/src/main/java/com/rwl/india/in/pan_card/App.java
@@ -0,0 +1,13 @@
+package com.rwl.india.in.pan_card;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
